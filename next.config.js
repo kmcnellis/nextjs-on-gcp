88719520a5b31e4e5865e0ddbb63ff6484a2b4b0
Kiana McNellis <kiana@kmcnellis.com>
2023-12-07 15:56:41 -0600

    Remove ISR cache path next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   output: "standalone",
   experimental: {
     isrMemoryCacheSize: 0,
-    incrementalCacheHandlerPath:
-      process.env.NODE_ENV === "production" ||
-      !!process.env.FIREBASE_DATABASE_EMULATOR_HOST
-        ? require.resolve("./lib/rtdb-cache.js")
-        : undefined,
   },
   generateBuildId: async () => {
     return process.env.BUILD_ID || "deadbeef";
